949871f74a1ac0d95a3d23b40b092c60bb062175
clai <clai-@hotmail.com>
2018-03-28 11:07:58 -0700

    initial
    Signed-off-by: clai <clai-@hotmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,11 +7,12 @@ MainWindow::MainWindow(QWidget *parent)
     , m_server(new MediaServer(this, 5150))
     , m_voiceChat(new VoiceChatController(this))
 {
+    QMediaPlayer* player = new QMediaPlayer;
+
     m_ui->setupUi(this);
     // Test for playing audio
-    connect(m_ui->actionAdd_file, &QAction::triggered, this, [this] (){
+    connect(m_ui->actionAdd_file, &QAction::triggered, this, [this, player] (){
        fileName = QFileDialog::getOpenFileName(this, tr("Open file"));
-       QMediaPlayer* player = new QMediaPlayer;
        player->setMedia(QUrl::fromLocalFile(fileName));
        player->setVolume(50);
        player->play();
@@ -19,6 +20,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(m_ui->actionStart_session, &QAction::triggered, m_voiceChat, &VoiceChatController::hostSession);
     connect(m_ui->actionJoin_session, &QAction::triggered, m_voiceChat, &VoiceChatController::joinSession);
+    connect(m_ui->playPauseBtn, SIGNAL(pressed()), this, SLOT(player->pause());
 }
 
 MainWindow::~MainWindow()
