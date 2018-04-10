611ae67ae9393315ed7e7cd8d179b767c30bca29
clai <clai-@hotmail.com>
2018-04-10 09:52:34 -0700

    moved some code around, split functions
    Signed-off-by: clai <clai-@hotmail.com>

--- src/mediaclient.cpp
+++ src/mediaclient.cpp
@@ -4,42 +4,36 @@ MediaClient::MediaClient(QObject *parent) : QObject(parent)
   , connected(false)
 {
     timer = new QTimer(this);
+
 }
 
 void MediaClient::connectClient()
 {
-
-    connect(&m_client_sock, static_cast<void(QAbstractSocket::*)(QAbstractSocket::SocketError)>(&QAbstractSocket::error),this,
-            [=](QAbstractSocket::SocketError socketError){
-        qInfo() << m_client_sock.errorString(); });
-
-    m_client_sock.connectToHost(ipAddress, 5150);
-    connected = true;
-
-    QByteArray ba = fileName.toLatin1();
-    const char *temp = ba.data();
-    m_client_sock.write(temp);
-
-    connect(&m_client_sock, SIGNAL(readyRead()), this, SLOT(readyRead()));
-    connect(timer, SIGNAL(timeout()), this, SLOT(closeFile()));
-    timer->start(2000);
-
-
+    if (!ipAddress.isEmpty()) {
+        QHostAddress serverIP(ipAddress);
+        m_client_usock.bind(serverIP, 7755);
+        connect(&m_client_usock, SIGNAL(readyRead()), this, SLOT(readPendingDatagrams()));
+        uconnected = true;
+    } else {
+        QMessageBox msgBox;
+        msgBox.setText("Please connect to server first.");
+        msgBox.exec();
+    }
 }
 
-void MediaClient::getReqInfo() {
+void MediaClient::connectToServer() {
     bool ok;
-    QString ip = QInputDialog::getText(Q_NULLPTR, tr("Download from Server"), tr("Server IP Address:"), QLineEdit::Normal, "127.0.0.1", &ok);
-    QString file = QInputDialog::getText(Q_NULLPTR, tr("Download from Server"), tr("Song Request:"), QLineEdit::Normal, "", &ok);
-    if (ok && !ip.isEmpty() && !file.isEmpty()) {
+    QString ip = QInputDialog::getText(Q_NULLPTR, tr("Connect to Server"), tr("Server IP Address:"), QLineEdit::Normal, "127.0.0.1", &ok);
+    if (ok && !ip.isEmpty()) {
         ipAddress = ip;
-        fileName = file;
-        connectClient();
+        connect(&m_client_sock, static_cast<void(QAbstractSocket::*)(QAbstractSocket::SocketError)>(&QAbstractSocket::error),this,
+                [=](QAbstractSocket::SocketError socketError) {
+            qInfo() << m_client_sock.errorString();
+        });
+        m_client_sock.connectToHost(ipAddress, 5150);
+        connected = true;
     }
 
-    QString filePath = "C:/Users/Matt/Music/Client_";
-    filePath.append(file);
-    outputFile.open(filePath.toStdString(), std::ios_base::binary);
 }
 
 void MediaClient::stream() {
@@ -57,15 +51,43 @@ void MediaClient::closeFile()
     outputFile.close();
 }
 
+void MediaClient::readPendingDatagrams()
+{
+    if (uconnected) {
+        while (m_client_usock.hasPendingDatagrams()) {
+            QNetworkDatagram datagram = m_client_usock.receiveDatagram();
+            processStream(datagram);
+        }
+    }
+}
 
+void MediaClient::processStream(QNetworkDatagram datagram) {
 
+}
 
+void MediaClient::joinGroup() {
 
+}
 
+void MediaClient::request() {
+    bool ok;
+    QString file = QInputDialog::getText(Q_NULLPTR, tr("Download from Server"), tr("Song Request:"), QLineEdit::Normal, "", &ok);
+    if (ok && !file.isEmpty()) {
+        fileName = file;
 
+        QByteArray ba = fileName.toLatin1();
+        const char *temp = ba.data();
+        m_client_sock.write(temp);
 
+        connect(&m_client_sock, SIGNAL(readyRead()), this, SLOT(readyRead()));
+        connect(timer, SIGNAL(timeout()), this, SLOT(closeFile()));
+        timer->start(2000);
+    }
+    QString filePath = "C:/Users/Matt/Music/Client_";
+    filePath.append(file);
+    outputFile.open(filePath.toStdString(), std::ios_base::binary);
 
-
+}
 
 
 
--- src/mediaclient.h
+++ src/mediaclient.h
@@ -1,13 +1,16 @@
 #ifndef MEDIACLIENT_H
 #define MEDIACLIENT_H
 
-#include <QObject>
-#include <QTcpSocket>
 #include <fstream>
 #include <QInputDialog>
 #include <QLineEdit>
-#include <string.h>
+#include <QMessageBox>
+#include <QNetworkDatagram>
+#include <QObject>
+#include <QTcpSocket>
 #include <QTimer>
+#include <QUdpSocket>
+#include <string.h>
 
 class MediaClient : public QObject
 {
@@ -16,22 +19,29 @@ class MediaClient : public QObject
 
 public:
     explicit MediaClient(QObject *parent = nullptr);
-    std::ifstream inputFile;
     void stream();
+    void processStream(QNetworkDatagram datagram);
+    void joinGroup();
+    void request();
     std::ofstream outputFile;
+    std::ifstream inputFile;
     QString fileName;
 
 public slots:
     void connectClient();
-    void getReqInfo();
+    void connectToServer();
+//    void getReqInfo();
     void readyRead();
     void closeFile();
+    void readPendingDatagrams();
 
 private:
     QTimer * timer;
     QTcpSocket m_client_sock;
+    QUdpSocket m_client_usock;
     QString ipAddress;
     bool connected;
+    bool uconnected;
 
 };
 
